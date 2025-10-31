94e3143830827edbb028856f62b900985d21a0ec
Zhaslanuly Sabyrzhan <sabyryshka@gmail.com>
2025-10-31 18:06:45 +0500

    feat: add initial version of Todo App

---
+++ README.md
@@ -0,0 +1,53 @@
+# 📝 Todo App
+
+A minimal and clean **Todo List** built with **HTML**, **CSS**, and **JavaScript**.  
+Add tasks, remove them, and keep everything saved in your browser using localStorage.
+
+## 🚀 Features
+
+- Add new todos
+- Delete existing todos
+- Automatically saved in `localStorage`
+- Responsive and minimalist design
+- Long text wraps neatly
+
+## 🧠 Tech Stack
+
+- HTML5
+- CSS3
+- Vanilla JavaScript
+
+## 📂 Project Structure
+
+```bash
+todo-app/
+│
+├─ src/
+│  ├─ index.html
+│  ├─ main.css
+│  └─ main.js
+│
+├─ README.md
+└─ .gitignore
+```
+
+## 💾 How to Run
+
+1. Clone this repository
+   ```bash
+   git clone git@github.com:sabyryshka/todo-app-basic.git
+   ```
+2. Open the folder
+   ```bash
+   cd todo-app
+   ```
+3. Open **index.html** in your browser
+
+That’s it — no build tools or dependencies needed.
+
+## 🧩 Possible Improvements
+
+- Edit todos
+- Mark tasks as completed
+- Filter by status (All / Active / Done)
+- Dark / light mode switch
---
+++ src/main.js
@@ -0,0 +1,48 @@
+const textField = document.querySelector('[data-todo-header-text-field]')
+const addTodoButton = document.querySelector('[data-todo-header-add-todo-button]')
+const todoList = document.querySelector('[data-todo-body-list]')
+
+const allTodo = JSON.parse(localStorage.getItem('allTodo')) || []
+
+const saveToLocalStorage = (key = 'allTodo') => {
+  localStorage.setItem(key, JSON.stringify(allTodo))
+}
+
+addTodoButton.addEventListener('click', () => {
+  if (textField.value.trim()) {
+    allTodo.push(textField.value)
+    textField.value = ''
+
+    saveToLocalStorage()
+    render()
+  }
+})
+
+const createElement = (tagName, textContent) => {
+  const element = document.createElement(tagName)
+  element.textContent = textContent
+
+  return element
+}
+
+const removeTodo = (index) => {
+  allTodo.splice(index, 1)
+  saveToLocalStorage()
+  render()
+}
+
+const render = () => {
+  todoList.innerHTML = ''
+
+  allTodo.forEach((todo, index) => {
+    const todoItem = createElement('li', todo)
+    const removeButton = createElement('button', '❌')
+
+    removeButton.addEventListener('click', () => removeTodo(index))
+
+    todoItem.append(removeButton)
+    todoList.append(todoItem)
+  })
+}
+
+render()
